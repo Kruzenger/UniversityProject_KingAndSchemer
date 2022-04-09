7974124c3cd1fb62ff8d0b948f8229e0700301fd
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2022-04-09 15:13:07 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "./Scripts/Tree.h"
+#include "./Scripts/IntriganActions.h"
+#include "./Scripts/AlgorithmPatterns.h"
+#include "./Scripts/ConspiracyAlgorithm.h"
+
+int main()
+{
+
+}
