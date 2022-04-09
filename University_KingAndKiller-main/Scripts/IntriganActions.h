c25f85eb46b732959e1a33fa4b93509c18571422
Kronte32 <90899611+Kronte32@users.noreply.github.com>
2022-04-09 15:31:47 +0300

    change
    переделал структуру проекта как-то

---
+++ University_KingAndKiller-main/Scripts/IntriganActions.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <./Main_structs.h>
+#include <./Tree.h>
+
+
+void make_Friends() {
+
+};
+
+void fall_Out() {
+
+};
+
+void become_a_Servant() {
+
+};
+
+void force() {
+
+};
+
+void conspire() {
+
+};
