636cd90894e27fd884d8bf779dcbbafd2cf26cd7
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2022-04-10 15:30:11 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,10 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#include "./Scripts/MainStructs.h"
 #include "./Scripts/Tree.h"
 #include "./Scripts/SchemerActions.h"
 #include "./Scripts/AlgorithmPatterns.h"
 #include "./Scripts/ConspiracyAlgorithm.h"
+#include "./Scripts/MainStructs.h"
+
+// struct person_st
+// {
+//     int name;
+
+//     struct person_st * Lord;
+
+//     struct person_st * friends[0];
+//     struct person_st * LastFriend;
+
+//     struct person_st * slaves[0];
+//     struct person_st * LastSlave;
+// };
+
+// struct tree_st
+// {
+//     Person * King;
+//     Person * Schemer;
+// };
+
+// struct friendPersone_st
+// {
+//     bool type;
+
+//     Person * person;
+//     // struct friendPersone_st * previous;
+//     struct friendPersone_st * next;
+// };
 
 int main()
 {
