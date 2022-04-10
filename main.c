2ea1f93a9061fc7b9fd4eb545e2d4bae08b9e1b9
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2022-04-10 22:49:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,34 +8,6 @@
 #include "./Scripts/ConspiracyAlgorithm.h"
 #include "./Scripts/MainStructs.h"
 
-// struct person_st
-// {
-//     int name;
-
-//     struct person_st * Lord;
-
-//     struct person_st * friends[0];
-//     struct person_st * LastFriend;
-
-//     struct person_st * slaves[0];
-//     struct person_st * LastSlave;
-// };
-
-// struct tree_st
-// {
-//     Person * King;
-//     Person * Schemer;
-// };
-
-// struct friendPersone_st
-// {
-//     bool type;
-
-//     Person * person;
-//     // struct friendPersone_st * previous;
-//     struct friendPersone_st * next;
-// };
-
 int main()
 {
 
