822ca2c548a1ecdab281e3fc0ba956defb7371c7
Kruzenger <33284905+Kruzenger@users.noreply.github.com>
2022-04-16 15:46:52 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,15 +4,22 @@
 
 #include "./Scripts/Tree.h"
 #include "./Scripts/SchemerActions.h"
-// #include "./Scripts/ConspiracyAlgorithm.h"
+#include "./Scripts/ConspiracyAlgorithm.h"
 #include "./Scripts/MainStructs.h"
+#include "./Scripts/AlgorithmPatterns.h"
 
 int main() {
     Tree tree;
+    
     Person *Persons;
     OperationLine *operationLines;
     FileWorker(&tree, &operationLines);
     TreeCreate(&tree, &Persons, operationLines);
+    FriendPerson ** FriendChains = FindChainOfFriendsToUs(tree.King, tree.Schemer->Lord, tree.n, false);
+    FriendPerson ** SlaveChains = FindChainOfFriendsToUs(tree.King, tree.Schemer, tree.n, true);
+    FreeChains(FriendChains);
+    FreeChains(SlaveChains);
     free(operationLines);
-    free(Persons);
+    FreeTree(&Persons, tree.n);
 }
+
